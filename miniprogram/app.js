70889f0bc157325c60099818da0dc003398cd99c
allen <yaokailun@ifanr.com>
2018-03-15 13:41:40 +0800

    Add More test

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,6 @@
-wx.BaaS = requirePlugin("myPlugin")
-
 App({
   onLaunch: function () {
+    wx.BaaS = requirePlugin("myPlugin")
     wx.BaaS.init('733b59d1b10ff4a37390')
     wx.BaaS.extendFunc(wx.login, wx.getUserInfo, wx.requestPayment)
   }
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,6 +1,51 @@
+let Product = new wx.BaaS.TableObject('product')
+let MyContentGroup = new wx.BaaS.ContentGroup(1521091635250470)
+let MyUser = new wx.BaaS.User()
+
 Page({
+  data: {
+    userInfo: {},
+    imageBase64: '',
+  },
+
+  showSuccessToast: function() {
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 1000
+    })
+  },
+
+  showFailToast: function() {
+    wx.showToast({
+      title: '失败',
+      icon: 'success',
+      duration: 1000
+    })
+  },
+
   login: function() {
-    wx.BaaS.login(wx.login, wx.getUserInfo)
+    wx.BaaS.login().then((res) => {
+      console.log('res', res)
+      this.setData({
+        userInfo: wx.BaaS.storage.get('userinfo')
+      })
+      this.showSuccessToast()
+    }).catch(err => {
+      console.log(err)
+      this.showFailToast()
+    })
+  },
+
+  logout: function() {
+    wx.BaaS.logout().then(() => {
+      this.setData({
+        userInfo: wx.BaaS.storage.get('userinfo')
+      })
+      this.showSuccessToast()
+    }, err => {
+      this.showFailToast()
+    })
   },
 
   pay: function() {
@@ -13,23 +58,90 @@ Page({
           }
           wx.BaaS.pay(params).then(res => {
             console.log(res)
+            this.showSuccessToast()
           }, err => {
             console.log(err)
+            this.showFailToast()
           })
         } else {
           wx.BaaS.login()
+          wx.showToast({
+            title: '再来一次',
+            icon: 'success',
+            duration: 1000
+          })
         }
       }
     })
   },
 
   getRecord: function() {
-    var Product = new wx.BaaS.TableObject('product')
     Product.get('5aa8f35b09a8050ae1bbce03').then(res => {
+      console.log('res', res.data)
+      this.showSuccessToast()
+    }, err => {
+      console.log(err)
+      this.showFailToast()
+    })
+  },
+
+  queryRecord: function() {
+    let query = new wx.BaaS.Query()
+    query.compare('created_by', '=', 100)
+    Product.setQuery(query).find().then(res => {
+      console.log(res.data)
+      this.showSuccessToast()
+    }, err => {
+      console.log(err)
+      this.showFailToast()
+    })
+  },
+
+  updateRecord: function() {
+    Product.getWithoutData('5aa8f35b09a8050ae1bbce03').set({created_by: 100}).update().then(res => {
       console.log('res', res.data)
     }, err => {
       console.log('err', err)
     })
+  },
+
+  geojson: function() {
+    let point = new wx.BaaS.GeoPoint(20, 20)
+    let product = Product.create()
+    product.set('gjson', point).save().then(res => {
+      console.log('res', res)
+      this.showSuccessToast()
+    }, err => {
+      console.log(err)
+      howFailToast()
+    })
+  },
+
+  getContent: function() {
+    MyContentGroup.getContent(1521091701839706).then(res => {
+      this.showSuccessToast()
+      console.log(res)
+    }, err => {
+      console.log(err)
+      showFailToast()
+    })
+  },
+
+  getUser: function() {
+    MyUser.get(61718290).then(res => {
+      console.log(res)
+      this.showSuccessToast()
+    }, err => {
+      console.log(err)
+      this.showFailToast()
+    })
+    // MyUser.find().then(res => {
+    //   console.log('res', res)
+    //   this.showSuccessToast()
+    // }, err => {
+    //   console.log(err)
+    //   this.showFailToast()
+    // })
   },
 
   uploadFile: function() {
@@ -46,6 +158,50 @@ Page({
 
         })
       }
+    })
+  },
+
+  getWXACode: function() {
+    const params = {
+      path: '../index/index?id=A',
+      width: 250
+    }
+    wx.BaaS.getWXACode('wxacode', params).then(res => {
+      this.setData({ imageBase64: res.image })
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
+  getRunData: function() {
+    wx.getWeRunData({
+      success(res) {
+        console.log('encrypted data:', res.encryptedData)
+        wx.checkSession({
+          success: function () {
+            wx.BaaS.wxDecryptData(res.encryptedData, res.iv, 'we-run-data').then(decrytedData => {
+              console.log('decryted data', decrytedData)
+              wx.showModal({ title: '微信步数', content: '控制台查看' })
+            }, err => {
+              showFailToast()
+              console.log(err)
+            })
+          },
+          fail: function () {
+            wx.BaaS.logout()
+            wx.BaaS.login()
+          }
+        })
+      }
+    })
+  },
+
+  reportTicket: function() {
+    wx.BaaS.wxReportTicket('ddd').then(res => {
+      this.showSuccessToast()
+    }, err => {
+      console.log(err)
+      this.showFailToast()
     })
   }
 })
