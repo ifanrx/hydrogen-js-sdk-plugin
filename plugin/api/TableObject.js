0575771a114826cbeb101b4390aff4f956f348e7
allen <yaokailun@ifanr.com>
2018-06-04 18:17:45 +0800

    Sync js SDK v1.4.0

--- plugin/api/TableObject.js
+++ plugin/api/TableObject.js
@@ -1,6 +1,9 @@
 const BaaS = require('./baas')
 const BaseQuery = require('./BaseQuery')
+const HError = require('./HError')
+const Query = require('./Query')
 const TableRecord = require('./TableRecord')
+const utils = require('./utils')
 
 class TableObject extends BaseQuery {
   constructor(tableID) {
@@ -12,12 +15,48 @@ class TableObject extends BaseQuery {
     return new TableRecord(this._tableID)
   }
 
-  delete(recordID) {
-    return BaaS.deleteRecord({tableID: this._tableID, recordID})
+  createMany(args) {
+    if (utils.isArray(args)) {
+      const params = {
+        tableID: this._tableID,
+        data: args
+      }
+      return BaaS.createRecordList(params)
+    } else {
+      throw new HError(605)
+    }
   }
 
-  getWithoutData(recordID) {
-    return new TableRecord(this._tableID, recordID)
+  delete(args) {
+    if (utils.isString(args) || Number.isInteger(args)) {
+      return BaaS.deleteRecord({tableID: this._tableID, recordID: args})
+    } else if (args instanceof Query) {
+      const params = {
+        tableID: this._tableID,
+        limit: this._limit,
+        offset: this._offset,
+        where: JSON.stringify(args.queryObject)
+      }
+      this._initQueryParams()
+      return BaaS.deleteRecordList(params)
+    } else {
+      throw new HError(605)
+    }
+  }
+
+  getWithoutData(args) {
+    if (utils.isString(args) || Number.isInteger(args)) {
+      return new TableRecord(this._tableID, args)
+    } else if (args instanceof Query) {
+      let queryObject = {}
+      queryObject.limit = this._limit
+      queryObject.offset = this._offset
+      queryObject.where = utils.cloneDeep(args.queryObject)
+      this._initQueryParams()
+      return new TableRecord(this._tableID, null, queryObject)
+    } else {
+      throw new HError(605)
+    }
   }
 
   get(recordID) {
