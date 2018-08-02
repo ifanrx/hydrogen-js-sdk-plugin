0f40684f97d3cd27083d8ca123278c49d9e8c9db
陈春寒 <381582176@qq.com>
2018-08-02 16:55:35 +0800

    使用 wxParser 展示页面

--- plugin/pages/veryrich/index.js
+++ plugin/pages/veryrich/index.js
@@ -1,18 +1,18 @@
 // plugin/pages/veryrich/index.js
 const API_HOST = 'https://sso.ifanr.com/'
+let wxParser = require('../../utils/wxParser/index');
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    adURL: ''
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     let {
       ad_config
     } = options
@@ -22,9 +22,25 @@ Page({
         ad_config: ad_config
       },
       success: (res) => {
-        this.setData({
-          adURL: res.data.redirect_url
-        })
+        let that = this
+        let html = res.data.html
+        try {
+          wxParser.parse({
+            bind: 'richText',
+            html: html,
+            target: that,
+            enablePreviewImage: false,
+            tapLink: (url) => {
+              // do nothing
+            }
+          });
+        } catch (e) {
+          wxParser.parse({
+            bind: 'richText',
+            html: `<div>HTML 解析错误: ${e.message}</div>`,
+            target: that,
+          });
+        }
       }
     })
   }
