87bb9ac0086fe02f67cfd5e0b27da26f5a133eee
zhangzeshuan <zhangzeshuan@ifanr.com>
2018-08-15 18:32:05 +0800

    添加 appName 配置，用来区分当前测试环境

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,5 +3,8 @@ App({
     wx.BaaS = requirePlugin('sdkPlugin')
     wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
     wx.BaaS.init('733b59d1b10ff4a37390')
-  }
+  },
+  config: {
+    appName: 'sdk',
+  },
 })
